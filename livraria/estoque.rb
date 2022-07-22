709cf7b73685fb2b9351534327e1c89802cd8dbf
Laylla Rodrigues <laylla.oliveira.rodrigues@hotmail.com>
2022-07-22 15:05:52 -0300

    tratando method_missing dinamicamente

--- livraria/estoque.rb
+++ livraria/estoque.rb
@@ -17,12 +17,15 @@ class Estoque
     @vendas.select { |l| l.tipo == tipo }.sort { |v1, v2| quantidade_de_vendas_por(v1, &campo) <=> quantidade_de_vendas_por(v2, &campo) }.last
   end
 
-  def livro_que_mais_vendeu_por(&campo)
-    que_mais_vendeu_por("livro", &campo)
-  end
-
-  def revista_que_mais_vendeu_por(&campo)
-    que_mais_vendeu_por("revista", &campo)
+  def method_missing(name)
+    matcher = name.to_s.match "(.+)_que_mais_vendeu_por_(.+)"
+    if matcher
+      tipo = matcher[1]
+      campo = matcher[2].to_sym
+      que_mais_vendeu_por(tipo, &campo)
+    else
+      super
+    end
   end
 
   def exporta_csv
